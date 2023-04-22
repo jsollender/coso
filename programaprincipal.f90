e9c419a1fd6750014c1125e9b5f2522696d422ff
javimalcob <javier.mercado.alcoba@mi.unc.edu.ar>
2023-04-22 16:35:49 -0300

    programa principal v2

--- programaprincipal.f90
+++ programaprincipal.f90
@@ -2,25 +2,37 @@ program parcial1
 
 !! CACA
 !! CACA PRUEBA GIT
-	use mod_prec
-	use mod_funciones
-	implicit none
-	
+    use mod_prec
+    use mod_funciones
+    use mod_metodos
+    implicit none
+    
 	!Declaracion de variables
-	real (wp)		:: t, a, b
-	integer (il)	:: i, fu
-	character (80)	:: file_datos
+	real (wp)		:: t, a0, b0 !variables del grafico
+    real (wp)       :: a, b   !extremos del intervalo incial metodo Biseccion
+    real (wp)       :: p0     !aprox. inicial de la raiz metodo Newton
+    real (wp)       :: x0, x1 !aproximaciones iniciales metodo Secante
+    real (wp)       :: tolx   !tolerancia del error relativo en x
+    real (wp)       :: toly   !tolerancia del error absoluto en y
+    integer (il)    :: maxite !maximo de iteracion permitidas
+	integer (il)	:: i, fu  !variable i iteracion, fu archivo
+	character (80)	:: file_datos !variable del nombre archivo del grafico
 	
-	file_datos = "datos.dat"
-	a = 1._wp
-	b = 2._wp
+	!-------------------------------------------------------------------------
+    !Parte II.b
+    file_datos = "p2-funciones.dat"
+	a0 = 1._wp
+	b0= 2._wp
 	
 	open(newunit=fu, file=file_datos)
 		write(fu, '(4A20)') "t", "x1(t)", "x2(t)", "DeltaX(t)"
 
 		do i=0, 600, 1
-			t = a + i*(b-a)/600._wp
+			t = a0 + i*(b0-a0)/600._wp
 			write (fu,'(4(X, ES20.13))') t, x1(t), x2(t), DeltaX(t) 
 		end do	
 	close (fu)
+
+    !-------------------------------------------------------------------------
+    
 end program parcial1
